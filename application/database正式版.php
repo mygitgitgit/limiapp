60ad6e2837e3e8febdd4938ac98d4ec6971b4224
mygitgitgit <33996028+mygitgitgit@users.noreply.github.com>
2018-08-13 11:40:41 +0800

    Update database正式版.php

--- application/database正式版.php
+++ application/database正式版.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.97.218.145',
+    'hostname'        => '',
     // 数据库名
     'database'        => 'limiapp',
     // 用户名
     'username'        => 'limiapp',
     // 密码
-    'password'        => 'YOUhong@limiapp888',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
